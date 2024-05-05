27b97840ae91cfa2519ddeeaf20bdb28e64eaaef
SAGOR1012 <ar.sagor212@gmail.com>
2024-05-05 11:12:43 +0600

    set delete operations on user bookings file

--- src/Pages/UserBookings/UserBookings.jsx
+++ src/Pages/UserBookings/UserBookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Routes/Provider/AuthProvider";
 import BookingTableRow from "./BookingTableRow";
+import Swal from "sweetalert2";
 
 const UserBookings = () => {
     const { user } = useContext(AuthContext);
@@ -16,6 +17,37 @@ const UserBookings = () => {
             .then(res => res.json())
             .then(data => setUserBookings(data));
     }, [])
+
+
+    /* delete function  */
+    const handleDelete = (id) => {
+        const porceed = confirm('Are you sure you want to delete it ?')
+        if (porceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+
+                        /* delete success alert */
+                        Swal.fire({
+                            position: "center",
+                            icon: "success",
+                            title: "Delete Successfully",
+                            showConfirmButton: false,
+                            timer: 1000
+                        });
+
+                        /* ui theke delte item clear kore dite hobe  */
+                        const remaining = userBookings.filter(booking => booking._id !== id)
+                        setUserBookings(remaining)
+                    }
+                })
+        }
+
+    }
     return (
         <div>
             <h1 className="text-5xl"> user booking list : {userBookings.length}</h1>
@@ -30,7 +62,7 @@ const UserBookings = () => {
                                     <input type="checkbox" className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
+                            <th>Image</th>
                             <th>Serevice Titile</th>
                             <th>Date</th>
                             <th>Price</th>
@@ -43,6 +75,7 @@ const UserBookings = () => {
                                 <BookingTableRow
                                     key={booking._id}
                                     booking={booking}
+                                    handleDelete={handleDelete}
                                 >
                                 </BookingTableRow>)
                         }
